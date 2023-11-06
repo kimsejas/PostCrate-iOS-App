0fea915993e52f73e83a7630f49004a5f8c18df3
kimsejas <sejaskm@gmail.com>
2023-11-06 14:19:53 -0500

    starter project

---
+++ postCrate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  postCrate
+//
+//  Created by Kimberly Sejas on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
