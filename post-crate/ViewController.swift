b40e5bcb62dfda39b4ae632b0f0c74b9b789ea0f
kimsejas <sejaskm@gmail.com>
2023-11-06 14:04:42 -0500

    added starter

---
+++ post-crate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  post-crate
+//
+//  Created by Kimberly Sejas on 11/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
